ffdf81fcfd1627fe55d54804a9f4b8d07febc872
Martin <mrtn.ritter@googlemail.com>
2022-11-01 02:18:04 +0100

    Add files via upload

---
+++ srvman.js
@@ -0,0 +1,10 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	// ns.tprint(ns.nFormat(ns.getPurchasedServerCost(ns.getPurchasedServerMaxRam()), '($ 0.00 a)'));
+
+	// ns.killall("srv-01");
+	// ns.deleteServer("srv-01");
+	// ns.purchaseServer("srv-01", (ns.getPurchasedServerMaxRam()));
+
+	
+}
